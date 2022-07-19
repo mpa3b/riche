f3edcbd89dc097c013b43714926a89f64494a983
gr0mi <im0rg@mail.ru>
2022-07-19 07:08:39 +0300

    task-63196 - Верхнее меню над баннером

---
+++ header.menu_ext.php
@@ -0,0 +1,23 @@
+<?php
+$aMenuLinks[] = [
+    'Магазин',
+    '/shop/',
+    [],
+    [
+        'FROM_IBLOCK' => true,
+        'IS_PARENT'   => false,
+        'DEPTH_LEVEL' => 1,
+    ],
+];
+
+
+$aMenuLinks[] = [
+    'Волосы',
+    '/shop/hair/',
+    [],
+    [
+        'FROM_IBLOCK' => true,
+        'IS_PARENT'   => false,
+        'DEPTH_LEVEL' => 1,
+    ],
+];
