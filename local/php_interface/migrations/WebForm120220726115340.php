1199dec485b5b10b2a7e4365db431219c43204af
gr0mi <im0rg@mail.ru>
2022-07-26 11:54:23 +0300

    task-63212 - Веб форма - Компонент вызова - Обработчик для отправки в Unisender

---
+++ local/php_interface/migrations/WebForm120220726115340.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class WebForm120220726115340 extends Version
+{
+    protected $description = "Бьюти рассылка в футере";
+
+    protected $moduleVersion = "3.30.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $formHelper = $helper->Form();
+        $formId = $formHelper->saveForm(array (
+  'NAME' => 'Бьюти рассылка',
+  'SID' => 'SIMPLE_FORM_1',
+  'BUTTON' => 'Подписаться',
+  'C_SORT' => '100',
+  'FIRST_SITE_ID' => NULL,
+  'IMAGE_ID' => NULL,
+  'USE_CAPTCHA' => 'Y',
+  'DESCRIPTION' => 'Подписывайся и получай самые интересные предложения первой!',
+  'DESCRIPTION_TYPE' => 'text',
+  'FORM_TEMPLATE' => '',
+  'USE_DEFAULT_TEMPLATE' => 'Y',
+  'SHOW_TEMPLATE' => NULL,
+  'MAIL_EVENT_TYPE' => 'FORM_FILLING_SIMPLE_FORM_1',
+  'SHOW_RESULT_TEMPLATE' => NULL,
+  'PRINT_RESULT_TEMPLATE' => NULL,
+  'EDIT_RESULT_TEMPLATE' => NULL,
+  'FILTER_RESULT_TEMPLATE' => NULL,
+  'TABLE_RESULT_TEMPLATE' => NULL,
+  'USE_RESTRICTIONS' => 'N',
+  'RESTRICT_USER' => '0',
+  'RESTRICT_TIME' => '0',
+  'RESTRICT_STATUS' => '',
+  'STAT_EVENT1' => 'form',
+  'STAT_EVENT2' => 'footer',
+  'STAT_EVENT3' => 'send',
+  'LID' => NULL,
+  'C_FIELDS' => '0',
+  'QUESTIONS' => '1',
+  'STATUSES' => '1',
+  'arSITE' => 
+  array (
+    0 => 's1',
+  ),
+  'arMENU' => 
+  array (
+    'ru' => 'Бьюти рассылка',
+    'en' => '',
+  ),
+  'arGROUP' => 
+  array (
+  ),
+  'arMAIL_TEMPLATE' => 
+  array (
+  ),
+));
+        $formHelper->saveStatuses($formId, array (
+  0 => 
+  array (
+    'CSS' => 'statusgreen',
+    'C_SORT' => '100',
+    'ACTIVE' => 'Y',
+    'TITLE' => 'DEFAULT',
+    'DESCRIPTION' => 'DEFAULT',
+    'DEFAULT_VALUE' => 'Y',
+    'HANDLER_OUT' => NULL,
+    'HANDLER_IN' => NULL,
+  ),
+));
+        $formHelper->saveFields($formId, array (
+  0 => 
+  array (
+    'ACTIVE' => 'Y',
+    'TITLE' => 'E-mail адрес',
+    'TITLE_TYPE' => 'text',
+    'SID' => 'SIMPLE_QUESTION_754',
+    'C_SORT' => '100',
+    'ADDITIONAL' => 'N',
+    'REQUIRED' => 'N',
+    'IN_FILTER' => 'Y',
+    'IN_RESULTS_TABLE' => 'Y',
+    'IN_EXCEL_TABLE' => 'Y',
+    'FIELD_TYPE' => 'text',
+    'IMAGE_ID' => NULL,
+    'COMMENTS' => '',
+    'FILTER_TITLE' => 'E-mail адрес',
+    'RESULTS_TABLE_TITLE' => 'E-mail адрес',
+    'ANSWERS' => 
+    array (
+      0 => 
+      array (
+        'MESSAGE' => ' ',
+        'VALUE' => '',
+        'FIELD_TYPE' => 'email',
+        'FIELD_WIDTH' => '0',
+        'FIELD_HEIGHT' => '0',
+        'FIELD_PARAM' => '',
+        'C_SORT' => '0',
+        'ACTIVE' => 'Y',
+      ),
+    ),
+    'VALIDATORS' => 
+    array (
+    ),
+  ),
+));
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
+
--- local/templates/riche/footer.php
+++ local/templates/riche/footer.php
@@ -9,6 +9,44 @@ global $USER, $APPLICATION;
 
 ?>
 
+<? $APPLICATION->IncludeComponent(
+    "bitrix:form",
+    "",
+    [
+        "AGREEMENT_URL" => "/about/privacy-policy/",
+        "AJAX_MODE" => "Y",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "3600",
+        "CACHE_TYPE" => "A",
+        "CHAIN_ITEM_LINK" => "",
+        "CHAIN_ITEM_TEXT" => "",
+        "EDIT_ADDITIONAL" => "N",
+        "EDIT_STATUS" => "N",
+        "IGNORE_CUSTOM_TEMPLATE" => "N",
+        "NOT_SHOW_FILTER" => ["", ""],
+        "NOT_SHOW_TABLE" => ["", ""],
+        "RESULT_ID" => $_REQUEST[RESULT_ID],
+        "SEF_MODE" => "N",
+        "SHOW_ADDITIONAL" => "N",
+        "SHOW_ANSWER_VALUE" => "N",
+        "SHOW_EDIT_PAGE" => "N",
+        "SHOW_LIST_PAGE" => "N",
+        "SHOW_STATUS" => "N",
+        "SHOW_VIEW_PAGE" => "N",
+        "START_PAGE" => "new",
+        "SUCCESS_URL" => "",
+        "USE_EXTENDED_ERRORS" => "N",
+        "VARIABLE_ALIASES" => [
+            "action" => "action"
+        ],
+        "WEB_FORM_ID" => "1"
+    ]
+); ?>
+
 </div>
 
 <div id="page--footer">
