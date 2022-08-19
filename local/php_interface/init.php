5851cb13a91579307141fdfa1d98ad0dc67788b3
Илья Петровский <ilya.petrovsky@yandex.ru>
2022-08-19 12:36:54 +0300

    Заготовка шаблона квиза для главной.

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,54 +1,59 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . "/local/vendor/autoload.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/local/vendor/autoload.php";
 
-use Bitrix\Main\Application;
-use Bitrix\Main\Config\Option;
-use Bitrix\Main\EventManager;
-use Bitrix\Main\Loader;
-use Local\Buffer;
+    use Bitrix\Main\Application;
+    use Bitrix\Main\Config\Option;
+    use Bitrix\Main\EventManager;
+    use Bitrix\Main\Loader;
+    use Local\Buffer;
 
-//region костыль для очистки буфера от лишних элементов
+    //region костыль для очистки буфера от лишних элементов
 
-Loader::registerAutoLoadClasses(
-    null,
-    [
-        'buffer' => '/local/classes/Buffer.php'
-    ]
-);
+    Loader::registerAutoLoadClasses(
+        null,
+        [
+            'buffer' => '/local/classes/Buffer.php'
+        ]
+    );
 
-$eventManager = EventManager::getInstance();
+    $eventManager = EventManager::getInstance();
 
-// очистка буфера
-$eventManager->addEventHandler(
-    'main',
-    'OnEndBufferContent',
-    [
-        Buffer::class,
-        "clean"
-    ]
-);
+    $eventManager->addEventHandler(
+        'main',
+        'OnEndBufferContent',
+        [
+            Buffer::class,
+            "clean"
+        ]
+    );
 
 //endregion
 
 //region глобальная константа DEBUG
 
-if (Option::get('main', 'update_devsrv') == 'Y') {
-    $debug = true;
-} else {
-    $debug = false;
-}
+    if (Option::get('main', 'update_devsrv') == 'Y') {
+        $debug = true;
+    }
+    else {
+        $debug = false;
+    }
 
-/**
- * Константа, определяющая режим работы сайта: отладочный, обычный. Значение задаётся через опцию "версия для отладки" в настройках обновления Битрикс.
- */
-define('DEBUG', $debug);
+    /**
+     * @const DEBUG  Константа, определяющая режим работы сайта: отладочный, обычный. Значение задаётся через опцию
+     *        "версия для отладки" в настройках обновления Битрикс.
+     */
+    define('DEBUG', $debug);
 
-$request = Application::getInstance()->getContext()->getRequest();
+//endregion
 
-/**
- * Константа, сообщающая, является ли текущий запрос аякс-запросом
- */
-define('IS_AJAX', $request->isAjaxRequest());
+//region глобальная константа IS_AJAX
+
+    $request = Application::getInstance()->getContext()->getRequest();
+
+    /**
+     * @const IS_AJAX Константа, сообщающая, является ли текущий запрос аякс-запросом
+     */
+    define('IS_AJAX', $request->isAjaxRequest());
 
 //endregion
