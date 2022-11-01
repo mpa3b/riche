13e58142022462dffe1979d4a7e002ed7487ad66
Илья Петровский <ilya.petrovsky@gmail.com>
2022-11-01 15:48:14 +0300

    Заготовка шаблона квиза для главной.

--- local/templates/riche/classes/Head.php
+++ local/templates/riche/classes/Head.php
@@ -11,73 +11,6 @@ class Head
 {
 
     /**
-     * Перечень MIME-типов содержимого
-     */
-    private const MIME_TYPES
-        = [
-
-            'txt'  => 'text/plain',
-            'htm'  => 'text/html',
-            'html' => 'text/html',
-            'php'  => 'text/html',
-            'css'  => 'text/css',
-            'js'   => 'application/javascript',
-            'json' => 'application/json',
-            'xml'  => 'application/xml',
-            'swf'  => 'application/x-shockwave-flash',
-            'flv'  => 'video/x-flv',
-
-            // images
-            'png'  => 'image/png',
-            'jpe'  => 'image/jpeg',
-            'jpeg' => 'image/jpeg',
-            'jpg'  => 'image/jpeg',
-            'gif'  => 'image/gif',
-            'bmp'  => 'image/bmp',
-            'ico'  => 'image/vnd.microsoft.icon',
-            'tiff' => 'image/tiff',
-            'tif'  => 'image/tiff',
-            'webp' => 'image/webp',
-
-            //vector
-            'svg'  => 'image/svg+xml',
-            'svgz' => 'image/svg+xml',
-
-            // archives
-            'zip'  => 'application/zip',
-            'rar'  => 'application/x-rar-compressed',
-            'exe'  => 'application/x-msdownload',
-            'msi'  => 'application/x-msdownload',
-            'cab'  => 'application/vnd.ms-cab-compressed',
-
-            // audio/video
-            'mp3'  => 'audio/mpeg',
-            'qt'   => 'video/quicktime',
-            'mov'  => 'video/quicktime',
-            'mp4'  => 'video/mp4',
-            'ovg'  => 'video/ogg',
-            'webm' => 'video/webm',
-
-            // adobe
-            'pdf'  => 'application/pdf',
-            'psd'  => 'image/vnd.adobe.photoshop',
-            'ai'   => 'application/postscript',
-            'eps'  => 'application/postscript',
-            'ps'   => 'application/postscript',
-
-            // ms office
-            'doc'  => 'application/msword',
-            'rtf'  => 'application/rtf',
-            'xls'  => 'application/vnd.ms-excel',
-            'ppt'  => 'application/vnd.ms-powerpoint',
-
-            // open office
-            'odt'  => 'application/vnd.oasis.opendocument.text',
-            'ods'  => 'application/vnd.oasis.opendocument.spreadsheet',
-
-        ];
-
-    /**
      * Определение типа содержимого для подстановки в *link as*
      *
      * @param $url
@@ -119,53 +52,20 @@ class Head
     }
 
     /**
-     * Определение типа содержимого для других функций, например для указания типа в *source type*
-     *
-     * @param $filepath
-     *
-     * @return string
-     */
-    public static function getMimeType($filepath): string
-    {
-
-        $mime_types = self::MIME_TYPES;
-
-        $ext = strtolower(pathinfo($filepath, PATHINFO_EXTENSION));
-
-        if (array_key_exists($ext, $mime_types)) {
-
-            return $mime_types[$ext];
-
-        }
-        else {
-            if (function_exists('finfo_open')) {
-
-                $finfo    = finfo_open(FILEINFO_MIME);
-                $mimetype = finfo_file($finfo, $filepath);
-
-                finfo_close($finfo);
-
-                return $mimetype;
-
-            }
-            else {
-
-                return 'application/octet-stream';
-
-            }
-        }
-
-    }
-
-    /**
      * Функция добавления внешнего ресурса в предзагрузку как *link preload*
      *
      * @param $url
      */
-    public static function addHeadPreloadAsset(string $url): void
+    public static function addHeadPreloadAsset(string $url, string $type): void
     {
 
-        Asset::getInstance()->addString('<link rel="preload" href="' . $url . '" as="' . self::getAssetType($url) . '" >');
+        if(!$type) {
+
+            $type = self::getAssetType($url);
+
+        }
+
+        Asset::getInstance()->addString('<link rel="preload" href="' . $url . '" as="' . $type . '" >');
 
     }
 
@@ -174,10 +74,16 @@ class Head
      *
      * @param $url
      */
-    public static function addHeadPrefetchAsset(string $url): void
+    public static function addHeadPrefetchAsset(string $url, string $type): void
     {
 
-        Asset::getInstance()->addString('<link rel="prefetch" href="' . $url . '" as="' . self::getAssetType($url) . '">');
+        if(!$type) {
+
+            $type = self::getAssetType($url);
+
+        }
+
+        Asset::getInstance()->addString('<link rel="prefetch" href="' . $url . '" as="' . $type . '">');
 
     }
 
