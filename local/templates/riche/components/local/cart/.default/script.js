a99dd554194b63f9bf6e98fb749452e945a8d41a
Илья Петровский <ilya.petrovsky@gmail.com>
2022-11-02 20:34:32 +0300

    Заготовка шаблона квиза для главной.

--- local/templates/riche/components/local/cart/.default/script.js
+++ local/templates/riche/components/local/cart/.default/script.js
@@ -2,4 +2,26 @@ $(() => {
 
     const element   = $('.cart--default');
 
+    let items = $.bxAjax(element.data('component')),
+        container = $('ul.items', element);
+
+    console.debug(items);
+
+    container.renderTemplate(items);
+
+    $(document).on(
+        'click',
+        '.cart--default button[data-action]',
+        (event) => {
+
+            event.preventDefault();
+
+            let data = $(event.target).data();
+
+            $.bxAjax(component, data);
+
+        }
+    );
+
+
 });
