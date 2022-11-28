c98091b57926ce3407bf94f75d851921ca0c295b
Илья Петровский <ilya.petrovsky@gmail.com>
2022-11-28 17:56:53 +0300

    Небольшие проблемы с композитом.
    При повторном обновлении перестаёт срабатывать слайдер проблем.

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,11 +1,11 @@
 <?php
-
-$arUrlRewrite = [
-    0 => [
-        'CONDITION' => '#^/shop/#',
-        'RULE'      => '',
-        'ID'        => 'bitrix:catalog',
-        'PATH'      => '/shop/index.php',
-        'SORT'      => 100,
-    ],
-];
+$arUrlRewrite=array (
+  0 => 
+  array (
+    'CONDITION' => '#^/shop/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/shop/index.php',
+    'SORT' => 100,
+  ),
+);
